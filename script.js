129fb36931a8b37fbe104b5bf1bc23de7ebbe553
lnbEli <137424458+lnbEli@users.noreply.github.com>
2024-03-23 21:08:40 +0100

    Create modal. Creat flex form wrap layout on modal

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
-const hello = "work hard";
+function Book() {}
+
+function addBookToLibrary() {}
+
+const book1 = {
+  author: "George Orwell",
+  title: "Homage to Catalonia",
+  pages: 378,
+  read: true,
+};
+
+const book2 = {
+  author: "Jonathan Swift",
+  title: "Gullivers Travels",
+  pages: 328,
+  read: true,
+};
+
+const book3 = {
+  author: "Albert Camus",
+  title: "The Stranger",
+  pages: 268,
+  read: false,
+};
+
+const myLibrary = [book1, book2, book3];
