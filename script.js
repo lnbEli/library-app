699b1dc30bec98d96f03e83f5f3cbe1fc48f6403
lnbEli <137424458+lnbEli@users.noreply.github.com>
2024-03-24 21:35:19 +0100

    Create sumbit function that adds inffo to new object stored in array using constructor function.

--- script.js
+++ script.js
@@ -1,7 +1,3 @@
-function Book() {}
-
-function addBookToLibrary() {}
-
 const book1 = {
   author: "George Orwell",
   title: "Homage to Catalonia",
@@ -24,3 +20,47 @@ const book3 = {
 };
 
 const myLibrary = [book1, book2, book3];
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const exitModalBtn = document.querySelector(".exit-btn");
+const openModalBtn = document.querySelector(".open-btn");
+const submitBtn = document.querySelector("#submit");
+
+exitModalBtn.addEventListener("click", closeModal);
+openModalBtn.addEventListener("click", openModal);
+submitBtn.addEventListener("click", submitForm);
+
+function Book(author, title, pages, read) {
+  this.author = author;
+  this.title = title;
+  this.pages = pages;
+  this.read = read;
+}
+
+function submitForm(e) {
+  e.preventDefault();
+  let author = document.querySelector("#author");
+  let title = document.querySelector("#title");
+  let pages = document.querySelector("#pages");
+  let read = document.querySelector("#already-read");
+  const newBook = new Book(author.value, title.value, pages.value, read.value);
+  myLibrary.push(newBook);
+  closeModal();
+}
+
+function closeModal() {
+  modal.setAttribute("hidden", "");
+  overlay.setAttribute("hidden", "");
+}
+
+function openModal() {
+  modal.removeAttribute("hidden", "");
+  overlay.removeAttribute("hidden", "");
+}
+
+function addBookToLibrary() {
+  const bookAuthor = document.querySelector(".book-author");
+  const bookName = document.querySelector(".book-name");
+  const bookPages = document.querySelector(".book-pages");
+  const bookRead = document.querySelector(".book-read");
+}
