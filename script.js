f19887c890260b5acf442dd7332d574748d9d68e
lnbEli <137424458+lnbEli@users.noreply.github.com>
2024-03-25 19:17:40 +0100

    Add function that loops through library array an adds each books details to DOM.Add event listne function to delete button of books currently in myLibrary.

--- script.js
+++ script.js
@@ -1,22 +1,27 @@
+// const bookAuthor = document.querySelector(".book-author");
+//   const bookName = document.querySelector(".book-name");
+//   const bookPages = document.querySelector(".book-pages");
+//   const bookRead = document.querySelector(".book-read");
+
 const book1 = {
   author: "George Orwell",
   title: "Homage to Catalonia",
   pages: 378,
-  read: true,
+  read: "yes",
 };
 
 const book2 = {
   author: "Jonathan Swift",
   title: "Gullivers Travels",
   pages: 328,
-  read: true,
+  read: "yes",
 };
 
 const book3 = {
   author: "Albert Camus",
   title: "The Stranger",
   pages: 268,
-  read: false,
+  read: "no",
 };
 
 const myLibrary = [book1, book2, book3];
@@ -26,9 +31,13 @@ const exitModalBtn = document.querySelector(".exit-btn");
 const openModalBtn = document.querySelector(".open-btn");
 const submitBtn = document.querySelector("#submit");
 
+addBookToDom();
+
 exitModalBtn.addEventListener("click", closeModal);
 openModalBtn.addEventListener("click", openModal);
 submitBtn.addEventListener("click", submitForm);
+
+// Constructor function to create books
 
 function Book(author, title, pages, read) {
   this.author = author;
@@ -37,14 +46,33 @@ function Book(author, title, pages, read) {
   this.read = read;
 }
 
+// Function to create new book object and adds it to library
+
 function submitForm(e) {
-  e.preventDefault();
   let author = document.querySelector("#author");
   let title = document.querySelector("#title");
   let pages = document.querySelector("#pages");
   let read = document.querySelector("#already-read");
-  const newBook = new Book(author.value, title.value, pages.value, read.value);
+
+  // Add basic form validation before preventing defualt.
+  if (author.value === "" || title.value === "") {
+    return;
+  }
+
+  e.preventDefault();
+  const newBook = new Book(
+    author.value,
+    title.value,
+    pages.value === "" ? "na" : pages.value,
+    read.value
+  );
   myLibrary.push(newBook);
+
+  addBookToDom();
+  author.value = "";
+  title.value = "";
+  pages.value = "";
+  read.value = "";
   closeModal();
 }
 
@@ -58,9 +86,41 @@ function openModal() {
   overlay.removeAttribute("hidden", "");
 }
 
-function addBookToLibrary() {
-  const bookAuthor = document.querySelector(".book-author");
-  const bookName = document.querySelector(".book-name");
-  const bookPages = document.querySelector(".book-pages");
-  const bookRead = document.querySelector(".book-read");
+// Loops through library array an adds each books details to DOM
+
+function addBookToDom() {
+  const bookList = document.querySelector(".book-list");
+  //Clears previously populated booklist
+  bookList.textContent = "";
+
+  for (let i = 0; i < myLibrary.length; i++) {
+    const htmlBlockOfBook = `<input type="button" class="remove-book-btn" data-id="${i}" value="&#x1f5d1"/><h3 class="book-author">${myLibrary[i].author}</h3>
+    <h4 class="book-name">${myLibrary[i].title}</h4>
+    <span>
+      <p>Pages:<span class="book-pages">${myLibrary[i].pages}</span></p>
+      <p>Read:<span class="book-read">${myLibrary[i].read}</span></p>
+    </span>`;
+
+    const li = document.createElement("li");
+    li.classList.add("book");
+    li.innerHTML = htmlBlockOfBook;
+    bookList.append(li);
+  }
+
+  addEventListeners();
+}
+
+// function to add event listner to current delete button of books currently in myLibrary
+// To be run every time the Library is updated or location in myLibrary array will be out of sync.
+
+function addEventListeners() {
+  const [...arrayOfDeleteButton] =
+    document.querySelectorAll(".remove-book-btn");
+
+  arrayOfDeleteButton.forEach((element) => {
+    element.addEventListener("click", () => {
+      myLibrary.splice(element.dataset.id, 1);
+      addBookToDom();
+    });
+  });
 }
